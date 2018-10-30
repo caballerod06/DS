c2622adba3f16190ea3050ecad45f4ba11e8557e
danica <caballerod06@gmail.com>
2018-10-30 14:11:05 +1300

    minor update

--- stack/src/main.c
+++ stack/src/main.c
@@ -1,8 +1,30 @@
 #include "stdio.h"
+#include "stack.h"
 
 int main(void)
 {
-	printf("This is test!!!");
+	Node *stack = NULL;
+
+	Push(&stack, 8);
+	Push(&stack, 11);
+	Push(&stack, 4);
+	Push(&stack, 1);
+	Push(&stack, 90);
+	Push(&stack, 12);
+
+	Print(stack);
+
+	printf("\nPop");
+	Pop(&stack);
+
+	printf("\nCurrent top is: %d", Top(stack));
+
+	printf("\nPop");
+	Pop(&stack);
+
+	Print(stack);
+
+	printf("\nCurrent top is: %d", Top(stack));
 
 	return 0;
 }
