e139327687d0fa9e6fdfe91bd1d34d0239224aea
danica <caballerod06@gmail.com>
2018-10-30 14:11:47 +1300

    minor update

--- stack/src/stack.c
+++ stack/src/stack.c
@@ -1,66 +1,67 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "stack.h"
 
 void Push(Node **stack, int val)
 {
-	Node *temp = NULL;
+    Node *temp = NULL;
 
-	temp = (Node *)malloc(sizeof(Node));
-	if (temp)
-	{
-		temp->data = val;
-	}
-	else
-	{
-		printf("\nCannot Allocate Memory!!");
-		return;
-	}
+    temp = (Node *)malloc(sizeof(Node));
+    if (temp)
+    {
+        temp->data = val;
+    }
+    else
+    {
+        printf("\nCannot Allocate Memory!!");
+	return;
+    }
 
-	if (NULL == *stack)
-	{
-		*stack = temp;
-		temp->next = NULL;
-	}
-	else
-	{
-		temp->next = *stack;
-		*stack = temp;
-	}
+    if (NULL == *stack)
+    {
+        *stack = temp;
+	temp->next = NULL;
+    }
+    else
+    {
+        temp->next = *stack;
+	*stack = temp;
+    }
 }
 
 void Pop(Node **stack)
 {
-	Node *temp = *stack;
-
-	if (NULL != *stack)
-	{
-		*stack = (*stack)->next;
-		free(temp);
-	}
-	else
-	{
-		printf("\nStack is empty!!");
-	}
-}
-
-int Top(Node *stack)
-{
-	return stack->data;
+    Node *temp = *stack;
+    
+    if (NULL != *stack)
+    {
+        *stack = (*stack)->next;
+	free(temp);
+    }
+    else
+    {
+        printf("\nStack is empty!!");
+    }
 }
 
 void Print(Node *stack)
 {
-	Node *temp = stack;
+    Node *temp = stack;
+    
+    if(NULL == temp)
+    {
+        printf("\nStack is empty!!");
+    }
 
-	if(NULL == temp)
-	{
-		printf("\nStack is empty!!");
-		return;
-	}
+    printf("\nStack contents: ");
+    while (NULL != temp)
+    {
+        printf("%d ", temp->data);
+	temp = temp->next;
+    }
+}
 
-	while (NULL != temp)
-	{
-		printf("%d ", temp->data);
-		temp = temp->next;
-	}
+int Top(Node *stack)
+{
+    return stack->data;
 }
